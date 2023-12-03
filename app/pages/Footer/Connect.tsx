b5229556ecd8e40c6ff02279039484b745bc2efb
Karthik Vanabhojana <Karthikvanabhojana1>
2023-12-02 23:59:37 -0500

    Commiting updating the responsiveness

--- app/pages/Footer/Connect.tsx
+++ app/pages/Footer/Connect.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import './Connect.css';
 import { RiTwitterXLine } from "react-icons/ri";
-
 import { MdMailOutline } from "react-icons/md";
 import { BsTelephone } from "react-icons/bs";
 import { HiOutlineLocationMarker } from "react-icons/hi";
@@ -13,100 +11,112 @@ import { FaYoutube } from "react-icons/fa";
 export default function Connect() {
     return (
         <div className='bg-white rounded-lg shadow m-4 dark:bg-white-800'>
-            <div className='connectHeading'>Connect</div>
-            <div className='connectDiscription'>Get In Touch</div>
-            <div className='connect py-8'>Have a question or need support? Contact us Today!</div>
-            <div className='contactDetails py-8'>
-                <div className='email'>
-                    <div className='testmail'>
-                        <MdMailOutline />
+            <div className='connectDiscription text-2xl font-extrabold mt-2'>Get In Touch</div>
+            <div className='connect text-lg py-4'>Have a question or need support? Contact us Today!</div>
+            <div className='contactDetails py-4 lg:py-8 flex flex-col lg:flex-row lg:justify-between'>
+                <div className='email text-center flex flex-col items-center mb-4 lg:mb-0'>
+                    <div className='testmail mb-2'>
+                        <MdMailOutline size={24} />
                     </div>
-                    <div className='emailHeading'>
+                    <div className='emailHeading font-semibold text-base'>
                         Email
                     </div>
-                    <div className='emaildiscription' > Send us an email and we'll get back to you promptly</div>
-                    <a href='mailto:hello@relume.io'>hello@relume.io</a>
+                    <div className='emaildiscription text-lg'>Send us an email, and we'll get back to you promptly</div>
+                    <a href='mailto:hello@relume.io' className='text-blue-500 mt-2'>hello@relume.io</a>
                 </div>
-                <div className='contact'>
-                    <BsTelephone />
-                    <div className='phoneHeading'>
+
+                <div className='contact text-center flex flex-col items-center mb-4 lg:mb-0'>
+                    <BsTelephone size={24} />
+                    <div className='phoneHeading font-semibold text-base'>
                         Phone
                     </div>
-                    <div className='phonediscription' > Give us a call and our friendly team will assist you</div>
-                    <a href='tel:+1(555)000-0000'>+1 (555)000-0000</a>
-
+                    <div className='phonediscription text-lg'>Give us a call, and our friendly team will assist you</div>
+                    <a href='tel:+1(555)000-0000' className='text-blue-500 mt-2'>+1 (555)000-0000</a>
                 </div>
-                <div className='address'>
-                    <HiOutlineLocationMarker />
-                    <div className='phoneHeading'>
+                <div className='address text-center flex flex-col items-center'>
+                    <HiOutlineLocationMarker size={24} />
+                    <div className='phoneHeading font-semibold text-base'>
                         Office
                     </div>
-                    <div className='addressdiscription' >Visit our office for personal consultation and tour</div>
-                    <a href='tel:+1(555)000-0000'>+1 (555)000-0000</a>
-
+                    <div className='addressdiscription text-lg'>Visit our office for a personal consultation and tour</div>
+                    <a href='geo:42.340082,-71.089488?q=Northeastern+University' className='text-blue-500 mt-2'>Northeastern University</a>
                 </div>
             </div>
-            <div className='connectsection py-10'>
-                <div className='logo'>
-                    <div className='logosName font-bold'>
+            <div className='connectsection py-10 flex flex-col lg:flex-row lg:justify-between'>
+                <div className='logo mb-4 lg:mb-0'>
+                    <div className='logosName font-bold text-lg'>
                         Logos
                     </div>
-                    <div className='logo-disc py-3'>
+                    <div className='logo-disc py-3 text-base'>
                         Stay up to date on the latest features and releases by joining our newsletter
                     </div>
-                    <div className='flex items-center gap-x-3'>
-                        <form className='formnewsletter py-3'>
-                            <input type="input" id="fname" name="fname"  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='Your email address'/>
-                            <input type="submit" value="Submit" id="newslettersubmit" className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"'/>
-                        </form>
 
+                    <div className='flex flex-col items-center lg:flex-row lg:gap-x-2'>
+                        <form className='formnewsletter mb-4 lg:mb-0'>
+                            <input
+                                type="input"
+                                id="fname"
+                                name="fname"
+                                className='block w-full rounded-md border-0 py-1.5 pr-2 text-gray-900 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 lg:text-lg lg:leading-6 mb-2 lg:mb-0'
+                                placeholder='Your email address'
+                            />
+                            <input
+                                type="submit"
+                                value="Submit"
+                                id="newslettersubmit"
+                                className='rounded-md bg-indigo-600 px-4 py-2 text-lg font-semibold text-white shadow-lg hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                            />
+                        </form>
                     </div>
                 </div>
-
-                <div className='column-1'> 
-                <div className='list'> 
-                <ul>
-                    <li><a className='py-1' href='#'>About</a></li>
-                    <li><a className='py-1'href='#'>Contact</a></li>
-                    <li><a className='py-1'href='#'>Link one</a></li>
-                    <li><a className='py-1'href='#'>Link one</a></li>
-                    <li><a className='py-1'href='#'>Link one</a></li>
-
-                </ul>
-
+                <div className='column-1'>
+                    <div className='list'>
+                        <ul className='list-none p-0'>
+                            <li className='icon py-1 text-blue-500'>
+                                <a href='#' className='ml-2'>About</a>
+                            </li>
+                            <li className='icon py-1 text-blue-500'>
+                                <a href='#' className='ml-2'>Contact</a>
+                            </li>
+                            <li className='icon py-1 text-blue-500'>
+                                <a href='#' className='ml-2'>Link one</a>
+                            </li>
+                            <li className='icon py-1 text-blue-500'>
+                                <a href='#' className='ml-2'>Link one</a>
+                            </li>
+                            <li className='icon py-1 text-blue-500'>
+                                <a href='#' className='ml-2'>Link one</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-
-                </div>
-                {/* <div className='column-2'> Column Two
-                <div className='list'> 
-                <ul>
-                    <li><a href='#'>Link one</a></li>
-                    <li><a href='#'>Link one</a></li>
-                    <li><a href='#'>Link one</a></li>
-                    <li><a href='#'>Link one</a></li>
-                    <li><a href='#'>Link one</a></li>
-
-                </ul>
-
-                </div>
-
-                </div> */}
                 <div className='socialmedia'>
-                    Socia Media
-                <ul>
-                <li><div className='icon py-1'><FaFacebook /><a  href='#'>Facebook</a></div></li>
-                    <li><div className='icon py-1'><IoLogoInstagram /><a href='#'>Instagram</a></div></li>
-                    <li><div className='icon py-1'><RiTwitterXLine /><a href='#'>Twitter</a></div></li>
-                    <li><div className='icon py-1'><FaLinkedin /><a href='#'>LinkedIn</a></div></li>
-                    <li><div className='icon py-1 '><FaYoutube /><a  href='#'>Youtube</a></div></li>
-
-                </ul>
+                    <p className='mr-4'>Social Media</p>
+                    <ul className='list-none p-0'>
+                        <li className='icon py-1 text-blue-500 flex items-center'>
+                            <FaFacebook className='mr-2' />
+                            <a href='#' className='ml-2'>Facebook</a>
+                        </li>
+                        <li className='icon py-1 text-blue-500 flex items-center'>
+                            <IoLogoInstagram className='mr-2' />
+                            <a href='#' className='ml-2'>Instagram</a>
+                        </li>
+                        <li className='icon py-1 text-blue-500 flex items-center'>
+                            <RiTwitterXLine className='mr-2' />
+                            <a href='#' className='ml-2'>Twitter</a>
+                        </li>
+                        <li className='icon py-1 text-blue-500 flex items-center'>
+                            <FaLinkedin className='mr-2' />
+                            <a href='#' className='ml-2'>LinkedIn</a>
+                        </li>
+                        <li className='icon py-1 text-blue-500 flex items-center'>
+                            <FaYoutube className='mr-2' />
+                            <a href='#' className='ml-2'>Youtube</a>
+                        </li>
+                    </ul>
                 </div>
-                
-
-                    
-                </div>
-
             </div>
+        </div>
     );
 }
+
