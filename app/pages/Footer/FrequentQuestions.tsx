a8853e81d08f201ea6e26a5fc9ee7aa0498fd03e
Karthik Vanabhojana <Karthikvanabhojana1>
2023-12-02 23:59:37 -0500

    Commiting updating the responsiveness

--- app/pages/Footer/FrequentQuestions.tsx
+++ app/pages/Footer/FrequentQuestions.tsx
@@ -2,55 +2,48 @@ import React from 'react'
 
 function FrequentQuestions() {
     return (
-        <div className='bg-white rounded-lg shadow m-4 dark:bg-white-800'>
-            <div className='text-3xl font-bold font-sans'>FAQ</div>
-            <p className='py-8'>Find answers to common questions about our roommate matching service</p>
-            <div className="border-t-2 border-gray-500 h-0 my-4 py-3">
-                <div className="flex">
-                    <div className="w-1/2 p-4 font-bold"> How does it work?
-                    </div>
-                    <div className="w-1/2 p-4">
-                        Our service matches potential roommates based on their university and interests. Once matched, we show them potential apartment listings they can move into together.  </div>
-                </div>
-            </div>
+       
+            <div className='bg-white rounded-lg shadow m-4 dark:bg-white-800'>
+  <div className='text-3xl font-bold font-sans'>FAQ</div>
+  <p className='py-8'>Find answers to common questions about our roommate matching service</p>
+
+  <div className="border-t-2 border-gray-500 my-1 py-3">
+    <div className="p-4 font-bold">How does it work?</div>
+    <div className="p-4">
+      Our service matches potential roommates based on their university and interests. Once matched, we show them potential apartment listings they can move into together.
+    </div>
+  </div>
+
+  <div className="border-t-2 border-gray-500 my-1 py-3">
+    <div className="p-4 font-bold">Is it free?</div>
+    <div className="p-4">
+      Yes, our roommate matching service is completely free to use.
+    </div>
+  </div>
+
+  <div className="border-t-2 border-gray-500 my-1 py-3">
+    <div className="p-4 font-bold">How do I sign up?</div>
+    <div className="p-4">
+      To sign up, simply create an account on our website and complete your profile. You'll then be able to start searching for potential roommates and apartments.
+    </div>
+  </div>
+
+  <div className="border-t-2 border-gray-500 my-1 py-3">
+    <div className="p-4 font-bold">How do I sign up?</div>
+    <div className="p-4">
+      To sign up, simply create an account on our website and complete your profile. You'll then be able to start searching for potential roommates and apartments.
+    </div>
+  </div>
+
+  <div className="border-t-2 border-gray-500 my-1 py-3">
+    <div className="p-4 font-bold">Can I search by location?</div>
+    <div className="p-4">
+      Yes, you can search for potential roommates and apartments by location. Simply enter the desired city or university name in the search bar.
+    </div>
+  </div>
+</div>
 
 
-            <div className="border-t-2 border-gray-500 h-0 my-16 py-3">
-                <div className="flex">
-                    <div className="w-1/2 p-4 font-bold">Is it free?
-                    </div>
-                    <div className="w-1/2 p-4">
-                        Yes, our roommate matching service is completely free to use  </div>
-                </div>
-            </div>
-
-
-            <div className="border-t-2 border-gray-500 h-0 my-16 py-3">
-                <div className="flex">
-                    <div className="w-1/2 p-4 font-bold"> How do I sign up?
-                    </div>
-                    <div className="w-1/2 p-4">
-                        To sign up, simply create an account on our website and complete your profile. You'll then be able to start searching for potential roommates and apartments</div>
-                </div>
-            </div>
-            <div className="border-t-2 border-gray-500 h-0 my-16 py-3">
-                <div className="flex">
-                    <div className="w-1/2 p-4 font-bold"> Can I search by location?
-                    </div>
-                    <div className="w-1/2 p-4">
-                        Yes, you can search for potential roommates and apartments by location. Simply enter the desired city or university name in the search bar. </div>
-                </div>
-            </div>
-            <div className="border-t-2 border-gray-500 h-0 my-16 py-3">
-                <div className="flex">
-                    <div className="w-1/2 p-4 font-bold"> Is my information secure?
-                    </div>
-                    <div className="w-1/2 p-4">
-                        We take the privacy and security of your information seriously. Rest assured that your data is protected and will not be shared with third parties. </div>
-                </div>
-            </div>
-
-        </div>
 
 
     )
