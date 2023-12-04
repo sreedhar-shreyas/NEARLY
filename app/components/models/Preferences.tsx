1a460c8b9eeed2f2b5d93067834700acdf2441cb
Karthik Vanabhojana <Karthikvanabhojana1>
2023-12-03 20:15:29 -0500

    Commit Upload Section

--- app/components/models/Preferences.tsx
+++ app/components/models/Preferences.tsx
@@ -1,52 +1,31 @@
-'use client';
+import React from 'react';
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
 
-import { 
-  FieldErrors, 
-  FieldValues, 
-  UseFormRegister 
-} from "react-hook-form";
-import { BiDollar } from "react-icons/bi";
-
-interface InputProps {
+interface SelectProps {
   id: string;
   label: string;
-  type?: string;
+  options: { value: string; label: string }[];
   disabled?: boolean;
-  formatPrice?: boolean;
+  register: UseFormRegister<FieldValues>;
   required?: boolean;
-  register: UseFormRegister<FieldValues>,
-  errors: FieldErrors
+  errors: FieldErrors;
 }
 
-const Input: React.FC<InputProps> = ({
+const Select: React.FC<SelectProps> = ({
   id,
   label,
-  type = "text", 
-  disabled, 
-  formatPrice,
+  options,
+  disabled,
   register,
   required,
   errors,
 }) => {
   return (
     <div className="w-full relative">
-      {formatPrice && (
-        <BiDollar
-          size={24}  
-          className="
-            text-neutral-700
-            absolute
-            top-5
-            left-2
-          "
-        />
-      )}
-      <input
+      <select
         id={id}
         disabled={disabled}
         {...register(id, { required })}
-        placeholder=" "
-        type={type}
         className={`
           peer
           w-full
@@ -60,12 +39,21 @@ const Input: React.FC<InputProps> = ({
           transition
           disabled:opacity-70
           disabled:cursor-not-allowed
-          ${formatPrice ? 'pl-9' : 'pl-4'}
+          pl-4
           ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
           ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
         `}
-      />
-      <label 
+      >
+        <option value="" disabled hidden>
+          Select an option
+        </option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <label
         className={`
           absolute 
           text-md
@@ -75,7 +63,7 @@ const Input: React.FC<InputProps> = ({
           top-5 
           z-10 
           origin-[0] 
-          ${formatPrice ? 'left-9' : 'left-4'}
+          left-4
           peer-placeholder-shown:scale-100 
           peer-placeholder-shown:translate-y-0 
           peer-focus:scale-75
@@ -86,7 +74,7 @@ const Input: React.FC<InputProps> = ({
         {label}
       </label>
     </div>
-   );
-}
- 
-export default Input;
+  );
+};
+
+export default Select;
