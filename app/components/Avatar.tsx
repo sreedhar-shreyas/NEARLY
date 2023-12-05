4bb0514c817c9b50e60ed033f0e98ea416361f4f
AnushaPoojary1998 <poojary.anu@northeastern.edu>
2023-12-04 19:50:04 -0500

    Individual listing and Listing reservation

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,15 +2,20 @@
 
 import Image from "next/image";
 
+interface AvatarProps {
+  src: string | null | undefined;
+}
 
-const Avatar = () => {
-    return ( 
-        <Image
-            className="rounded-full"
-            height="30"
-            width="30"
-            alt="ProfiilePic" src={"/images/placeholder.jpg"}        />
-     );
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return ( 
+    <Image 
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src={src || '/images/placeholder.jpg'}
+    />
+   );
 }
  
 export default Avatar;
