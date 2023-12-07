43e3b4107a94fdf6279e69f921e73be3d092a8d3
Spoorthikulkarni01 <kulkarni.sp@northeastern.edu>
2023-12-06 20:08:21 -0500

    homefeatures

--- app/landingpage/LandingPage.tsx
+++ app/landingpage/LandingPage.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import Connect from "./footer/Connect";
 import FrequentQuestions from "./footer/FrequentQuestions";
 import Hero from "./herosection/Hero";
+import Index from "./homefeatures/index";
+
 
 
 export default function LandingPage(){
     return (<>
     <Hero/>
     <div>Landing Page</div>
+    <Index/>
     <Connect/>
     <FrequentQuestions/>
+
     
     </>
       
