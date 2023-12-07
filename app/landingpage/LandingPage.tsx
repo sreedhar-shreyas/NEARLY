7e65f0575a29827818d2216d178a42e44efed8e1
AnushaPoojary1998 <poojary.anu@northeastern.edu>
2023-12-06 19:58:04 -0500

    Start now updated

--- app/landingpage/LandingPage.tsx
+++ app/landingpage/LandingPage.tsx
@@ -3,12 +3,13 @@ import Connect from "./footer/Connect";
 import FrequentQuestions from "./footer/FrequentQuestions";
 import Hero from "./herosection/Hero";
 import StartNow from "./startnow/startnow";
+import Safety from "../safetytips/page";
 
 
 export default function LandingPage(){
     return (<>
     <Hero/>
-        
+            
     <div>     
            
         Landing Page
--- app/landingpage/startnow/startnow.tsx
+++ app/landingpage/startnow/startnow.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
 
-
 const StartNow: React.FC = () => {
   return (
-    <div className="w-full h-full relative ">
-      <div className="w-1440 h-1601 absolute top-0 left-0 bg-white rounded-lg shadow m-4 dark:bg-white-800"></div>
-      <div className="absolute w-3309 h-1852 left-neg-927 top-676" style={{ width: '3309px', height: '1852px', left: '-927px', top: '676px', position: 'absolute' }}>
-        <div className="absolute w-1854 h-1850 left-0 top-0 bg-gradient-to-b from-purple-500 via-transparent to-transparent shadow-2xl rounded-full blur-[100px]" style={{ width: '1854px', height: '1850px', left: '0px', top: '0px', position: 'absolute', background: 'radial-gradient(50.68% 50.68% at 75.52% 88.61%, rgba(210, 97, 255, 0.79) 0%, rgba(210, 97, 255, 0) 100%)', boxShadow: '100px 100px 100px', borderRadius: '9999px', filter: 'blur(100px)' }}></div>
-        <div style={{ width: '1850px', height: '1852px', left: '3309px', top: '0px', position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', background: 'radial-gradient(45.53% 45.53% at 66.14% 69.35%, rgba(0, 218, 255, 0.79) 0%, rgba(0, 218, 255, 0) 100%)', boxShadow: '100px 100px 100px', borderRadius: '9999px', filter: 'blur(100px)' }}></div>
-      </div>
-      <img style={{ width: '1246px', height: '699px', left: '97px', top: '1071px', position: 'absolute', boxShadow: '0px 4px 28.799999237060547px 2px rgba(0, 0, 0, 0.25)' }} src="Screenshot.png" alt="Your Image Alt Text" />
-      <div className="absolute w-1193 h-128 left-97 top-439 text-center" style={{ width: '1193px', height: '128px', left: '97px', top: '439px', position: 'absolute', textAlign: 'center' }}>
-        <span style={{ color: 'black', fontSize: '43.20px', fontFamily: 'Neurial Grotesk', fontWeight: '400', wordWrap: 'break-word' }}> </span>
-        <span style={{ color: 'black', fontSize: '43.20px', fontFamily: 'Swear Display', fontStyle: 'italic', fontWeight: '800', wordWrap: 'break-word' }}>no more drama, just vibes.<br /></span>
-        <span style={{ color: 'black', fontSize: '43.20px', fontFamily: 'Neurial Grotesk', fontWeight: '400', wordWrap: 'break-word' }}> Your perfect roomie is just a tap away.</span>
-      </div>
-      <div style={{ width: '1101.60px', left: '169px', top: '671px', position: 'absolute', textAlign: 'center' }}>
-        <span style={{ color: '#080808', fontSize: '43.20px', fontFamily: 'Neurial Grotesk', fontWeight: '400', wordWrap: 'break-word' }}>it’s time to say</span>
-        <span style={{ color: '#080808', fontSize: '43.20px', fontFamily: 'Swear Display', fontStyle: 'italic', fontWeight: '800', wordWrap: 'break-word' }}> peace out</span>
-        <span style={{ color: '#080808', fontSize: '43.20px', fontFamily: 'Neurial Grotesk', fontWeight: '400', wordWrap: 'break-word' }}> to all the group chat mayhem</span>
-      </div>
-      <div className="absolute h-232.54 left-66 top-96.46" style={{ height: '232.54px', left: '66px', top: '96.46px', position: 'absolute' }}>
-        <div style={{ width: '1255px', left: '0px', top: '0px', position: 'absolute', textAlign: 'center', color: '#080808', fontSize: '128px', fontFamily: 'Swear Display', fontStyle: 'italic', fontWeight: '600', lineHeight: '133.12px', letterSpacing: '1.28px', wordWrap: 'break-word' }}>nearly</div>
-        <div style={{ width: '643px', left: '306px', top: '173.54px', position: 'absolute' }}>
-          <span style={{ color: '#080808', fontSize: '24px', fontFamily: 'Neurial Grotesk', fontWeight: '400', lineHeight: '58.24px', letterSpacing: '0.56px', wordWrap: 'break-word' }}>a </span>
-          <span style={{ color: '#FF6600', fontSize: '32px', fontFamily: 'Swear Display', fontStyle: 'italic', fontWeight: '700', lineHeight: '58.24px', letterSpacing: '0.56px', wordWrap: 'break-word' }}>Northeastern University only</span>
-          <span style={{ color: '#080808', fontSize: '24px', fontFamily: 'Neurial Grotesk', fontWeight: '400', lineHeight: '58.24px', letterSpacing: '0.56px', wordWrap: 'break-word' }}> Roommate Finder. </span>
+    <div className="overflow-hidden bg-white py-0 sm:py-20">
+      <div className="bg-white">
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+            <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
+          </div>
+          <div className="mx-auto max-w-2xl py-0 sm:py-2 lg:py-48">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="text-#080808 relative px-3 py-1 text-lg leading-6">presenting</div>
+            </div>
+            <div className="text-center">
+              <h1 className="font-swear text-4xl font-bold italic tracking-tight text-gray-900 sm:text-6xl" style={{ color: "#080808", fontSize: "128px", fontFamily: "Swear Display", fontStyle: "italic", fontWeight: 600, lineHeight: "133.12px", letterSpacing: "1.28px", wordWrap: "break-word" }}>nearly</h1>
+
+              <div><span style={{ color: "#080808", fontSize: "24px", fontFamily: "Neurial Grotesk", fontWeight: 400, lineHeight: "58.24px", letterSpacing: "0.56px", wordWrap: "break-word" }}>a </span><span style={{ color: "#FF6600", fontSize: "32px", fontFamily: "Swear Display", fontStyle: "italic", fontWeight: 700, lineHeight: "58.24px", letterSpacing: "0.56px", wordWrap: "break-word" }}>Northeastern University only</span><span style={{ color: "#080808", fontSize: "24px", fontFamily: "Neurial Grotesk", fontWeight: 400, lineHeight: "58.24px", letterSpacing: "0.56px", wordWrap: "break-word" }}> Roommate Finder. </span></div>
+
+              <div>
+                <p className="font-swear text-xl font-bold italic tracking-tight text-gray-900" style={{ color: "#080808", fontSize: "43px", fontFamily: "Swear Display", fontStyle: "italic", fontWeight: 600, lineHeight: "133.12px", letterSpacing: "1.28px", wordWrap: "break-word" }}>no more drama, just vibes</p>
+
+                <p className="font-Neurial Grotesk text-xl tracking-tight text-gray-900" style={{ color: "#080808", fontSize: "43px", fontFamily: "Swear Display", fontWeight: 500, lineHeight: "133.12px", letterSpacing: "0.20px", wordWrap: "break-word" }}>Your perfect roomie is just a tap away.</p>
+
+                <span className="text-lg text-gray-600" style={{ color: "#080808", fontSize: "30px", fontFamily: 'Swear Display', fontWeight: 500 }}> time to say </span>
+                <span className="text-lg italic text-gray-600" style={{ color: "#080808", fontSize: "30px", fontFamily: 'Swear Display', fontWeight: 800 }}> peace out</span>
+                <span className="text-lg text-gray-600" style={{ color: "#080808", fontSize: "30px", fontFamily: 'Swear Display', fontWeight: 500 }}> to all the group chat mayhem </span>
+
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a href="#" className="w-52 rounded-md bg-fuchsia-400 px-5 py-3 text-lg font-bold italic text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">start</a>
+                </div>
+              </div>
+            </div>
+
+            <img className="rounded-md" style={{ width: "1246px", height: "699px", left: "130px", top: "900px", position: "absolute", boxShadow: "0px 4px 28.799999237060547px 2px rgba(0, 0, 0, 0.25)" }} src="Screenshot.png" alt="Your Image Alt Text" />
+
+            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+              <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div style={{ width: '1101.60px', left: '148px', top: '72px', position: 'absolute', textAlign: 'center', color: '#080808', fontSize: '20px', fontFamily: 'Neurial Grotesk', fontWeight: '400', wordWrap: 'break-word' }}>presenting</div>
-      <div style={{ width: '300px', height: '60px', left: '570px', top: '831px', position: 'absolute', background: 'linear-gradient(0deg, rgba(210, 97, 255, 0.79) 0%, rgba(210, 97, 255, 0.79) 100%), radial-gradient(50.68% 50.68% at 75.52% 88.61%, rgba(210, 97, 255, 0.79) 0%, rgba(210, 97, 255, 0) 100%), radial-gradient(45.53% 45.53% at 66.14% 69.35%, rgba(0, 218, 255, 0.79) 0%, rgba(0, 218, 255, 0) 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '20px' }}></div>
-      <div style={{ left: '632px', top: '831px', position: 'absolute', textAlign: 'center', color: 'white', fontSize: '43.20px', fontFamily: 'Swear Display', fontStyle: 'italic', fontWeight: '800', wordWrap: 'break-word' }}>start now</div>
     </div>
   );
 };
