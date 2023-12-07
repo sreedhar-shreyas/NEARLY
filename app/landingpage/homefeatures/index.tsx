43f9f4816c4383be8331298452aa433a4cf1ffdf
Spoorthikulkarni01 <kulkarni.sp@northeastern.edu>
2023-12-06 23:21:22 -0500

    Homelandingsection

---
+++ app/landingpage/homefeatures/index.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import '../../globals.css';
+
+
+
+export default function Index() {
+  return (
+    <div className='main-container w-[1440px] h-[2284px] relative mx-auto my-0'>
+      <div className='w-[1440px] h-[768px] relative overflow-hidden z-[9] mt-[1516px] mr-0 mb-0 ml-0'>
+        <div className='w-[1440px] h-[1024px] absolute top-0 left-0 z-10'>
+          <div className='w-[1440px] h-[1024px] bg-[#fff] absolute top-0 left-0 z-[11]' />
+        </div>
+        <div className='w-[590px] h-[592px] bg-[url(/images/friends.png)] bg-cover bg-no-repeat absolute top-0 left-[953px] z-[12]' />
+        <span className="h-[39px] font-['Neurial_Grotesk'] text-[37px] font-normal leading-[38.488px] text-[#080808] tracking-0.37px absolute top-[99px] left-[170px] text-left z-[15]">
+          Fine-Tuned Living, Closer Than Ever
+        </span>
+        <div className='w-[394px] h-[282px] absolute top-[103px] left-[170px] z-[13]' />
+        <span className="w-[391px] h-[144px] font-['Arial'] text-[18px] font-normal leading-[28.8px] text-[#080808] absolute top-[199px] left-[170px] text-left z-[16]">
+          Tailor your search to be right where you want it, ensuring that your potential roommates and apartments are conveniently located. Whether you want to be steps away from classes or a short commute, Nearly helps you find the perfect balance between academic and living spaces.
+        </span>
+        <div className='w-[394px] h-[18px] rounded-[4px] absolute top-[367px] left-[170px] z-[14]' />
+        <span className="h-[18px] font-['Arial'] text-[15px] font-normal leading-[18px] text-[#080808] absolute top-[367px] left-[170px] text-left z-[17]">
+        </span>
+      </div>
+      <div className='w-full h-[33.63%] absolute top-0 left-0 overflow-hidden z-[18]'>
+        <div className='w-[1440px] h-[1024px] absolute top-0 left-0 z-[19]'>
+          <div className='w-[1440px] h-[1024px] bg-[#fff] absolute top-0 left-0 z-20' />
+        </div>
+        <div className='bg-[url(/images/frame.png)] bg-cover bg-no-repeat absolute top-[17px] bottom-[56px] left-[839px] right-[189px] z-[26]' />
+        <div className='bg-[#fff] rounded-[40px] absolute top-[36px] bottom-[69px] left-[853px] right-[204px] overflow-hidden z-[27]'>
+          <span
+            className="w-[391px] h-[144px] font-['Arial'] text-[18px] font-normal leading-[28.8px] text-[rgba(8,8,8,0.9)] absolute top-[267px] left-[860px] text-left z-[29]"
+            style={{ overflowY: 'auto', maxHeight: '200px', height: '100%' }}
+          >
+            
+            🔍 Swipe Right if You:
+
+          Have a knack for turning ordinary moments into extraordinary memories.
+          Can share a laugh over burnt attempts at cooking without setting off the smoke alarm.
+          Believe that cleaning can be a team sport (with extra points for creativity).
+          Appreciate the delicate balance between personal space and spontaneous dance parties.
+          💖 Get ready to embark on an apartment adventure with your ideal roomie match! Whether you're in it for the laughs, the shared Netflix binges, or the mutual appreciation for quirky decor, Nearly is here to make your cohabitation dreams come true. 🚀🏠
+
+            </span>
+        </div>
+        <span className="w-[404px] h-[77px] font-['Neurial_Grotesk'] text-[37px] font-normal leading-[38.488px] text-[#080808] tracking-0.37px absolute top-[99px] left-[170px] text-left z-[23]">
+          Match with Potential Roomates
+        </span>
+        <div className='w-[394px] h-[282px] absolute top-[103px] left-[170px] z-[21]' />
+        <span className="w-[391px] h-[144px] font-['Arial'] text-[18px] font-normal leading-[28.8px] text-[#080808] absolute top-[195px] left-[170px] text-left z-[24]">
+          Scroll through potential roommates and apartments in your area, swiping right for the perfect match and left for those that don't quite make the cut. Our intuitive interface makes finding the ideal living situation as easy as finding a date on your favorite dating app!
+        </span>
+        <span
+          className="w-[391px] h-[144px] font-['Arial'] text-[18px] font-normal leading-[28.8px] text-[rgba(8,8,8,0.9)] absolute top-[267px] left-[860px] text-left z-[29]"
+          style={{ overflowY: 'auto', maxHeight: '200px', height: '100%' }}
+        >
+          
+          🔍 Swipe Right if You:<br></br>
+
+          1. Have a knack for turning ordinary moments into extraordinary memories.<br></br>
+          2. Can share a laugh over burnt attempts at cooking without setting off the smoke alarm.<br></br>
+          3. Believe that cleaning can be a team sport (with extra points for creativity).<br></br>
+          4. Appreciate the delicate balance between personal space and spontaneous dance parties.<br></br>
+          💖 Get ready to embark on an apartment adventure with your ideal roomie match! Whether you're in it for the laughs, the shared Netflix binges, or the mutual appreciation for quirky decor, Nearly is here to make your cohabitation dreams come true. 🚀🏠<br></br>
+
+ #SwipeRightForRoomies
+
+        </span>
+        <div className='w-[394px] h-[18px] rounded-[4px] absolute top-[367px] left-[170px] z-[22]' />
+        <span className="h-[18px] font-['Arial'] text-[15px] font-normal leading-[18px] text-[#080808] absolute top-[367px] left-[170px] text-left z-[25]">
+        </span>
+      </div>
+      <div className='w-full h-[33.63%] absolute top-[32.75%] left-0 overflow-hidden'>
+        <div className='w-[1440px] h-[1024px] absolute top-0 left-0 z-[1]'>
+          <div className='w-[1440px] h-[1024px] bg-[#fff] absolute top-0 left-0 z-[2]' />
+        </div>
+        <div className='w-[590px] h-[592px] bg-[url(/images/page.png)] bg-cover bg-no-repeat absolute top-0 left-[-29px] z-[3]' />
+        <div className='w-[394px] h-[282px] absolute top-[103px] left-[170px] z-[4]' />
+        <span className="h-[39px] font-['Neurial_Grotesk'] text-[37px] font-normal leading-[38.488px] text-[#080808] tracking-0.37px absolute top-[103px] left-[971px] text-left z-[6]">
+          Tailored Connections for Seamless Co-Living
+        </span>
+        <span className="w-[391px] h-[144px] font-['Arial'] text-[18px] font-normal leading-[28.8px] text-[#080808] absolute top-[203px] left-[971px] text-left z-[7]">
+          Say goodbye to awkward living situations! Our cutting-edge SmartMatch Algorithm considers your lifestyle, preferences, and habits to connect you with roommates who share your vibe. Whether you're a night owl, a fitness enthusiast, or a Netflix binge-watcher, Nearly ensures you'll be harmonizing with the perfect roomie.
+        </span>
+        <div className='w-[394px] h-[18px] rounded-[4px] absolute top-[367px] left-[170px] z-[5]' />
+        <span className="h-[18px] font-['Arial'] text-[15px] font-normal leading-[18px] text-[#080808] absolute top-[367px] left-[170px] text-left z-[8]">
+        </span>
+      </div>
+    </div>
+  );
+}
